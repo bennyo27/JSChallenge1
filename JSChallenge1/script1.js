6740b93622ce946548b9c8aba02f9e6f3ea9ed6e
Benny Oseguera <39509363+bennyo27@users.noreply.github.com>
2018-05-29 14:38:32 -0700

    Add files via upload

---
+++ JSChallenge1/script1.js
@@ -0,0 +1,9 @@
+function changeBackground(){
+                
+                if(document.body.style.backgroundColor == "grey"){
+                  document.body.style.backgroundColor = "purple";
+                }
+                else {
+                    document.body.style.backgroundColor = "grey";
+                }
+            }
